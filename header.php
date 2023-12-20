442549583d17486f266969de239a1c7f55cfc294
Puhpine4915 <thomas.molenaar01@gmail.com>
2023-12-20 20:14:33 +0100

    mooiere header

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="header">
+            <a href="index.php">
+                <img src="images/NGLogo.png" alt="NGlogo" class="logoIMG">
+            </a>
+            <div class="headerLinks">
+                <div>
+                    <a href="browse.php">Graphics cards</a>
+                </div>
+                <div>
+                    <a href="browse.php">Motherboard</a>
+                </div>
+                <div>
+                    <a href="browse.php">Processors</a>
+                </div>
+                <div>
+                    <a href="browse.php">SSD</a>
+                </div>
+                <div>
+                    <a href="browse.php">RAM</a>
+                </div>
+            </div>
+        </div>
